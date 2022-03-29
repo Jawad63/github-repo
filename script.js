5ca2e13f6a2f044226bc09dd62bf003daec8cd72
Jawad63 <jawadqureshi111@gmail.com>
2022-03-29 16:29:15 +0200

    added if else statement

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
 const newName = "haider";
 const nextName = "Anees";
 console.log(newName + nextName);
+
+
+if (nextName === newName) {
+   console.log("something is wrong");
+} else {
+   console.log("still don't know");
+}
